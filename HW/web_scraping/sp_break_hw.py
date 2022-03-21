2f0563280425603701e86e7d0c2ae93f9d81c152
Towerhint <zc29@illinois.edu>
2022-03-20 23:37:49 -0500

    update

--- HW/web_scraping/sp_break_hw.py
+++ HW/web_scraping/sp_break_hw.py
@@ -1,6 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-file = open('raw_script_urls.txt')
+
+# Extract all the files 
+
+file = open('/Users/fafnir/Documents/GitHub/is310_final_project/HW/web_scraping/raw_script_urls.txt', 'r')  # Note that the path may need some changes(may add pathlib in the future)
+url = []
 for item in file:
-    print(item)
+    parts = item.split(' +++$+++ ')
+    url.append((parts[1].strip(), parts[2].strip()))
+
+# Use web-scraping to get all data and save them in a single file
+# There are 617 urls, to save time, we only scrape the first 50...
+
+# Request the pages
+
+def getText(url):
+    response = requests.get(url=url)
+    response.raise_for_status()
+    return response.text
+
+# We need to get the body from these texts
+
+for item in url[0]:
+    text = getText(item[1])
+    document = BeautifulSoup(text, "html.parser")
+    print(document)
